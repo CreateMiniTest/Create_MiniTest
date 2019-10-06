6b4a3ee518e6635fee6520ea4ee6bf0db1eb6b5c
alexander Plettinx <alexander.plettinx@student.howest.be>
2019-10-06 23:31:54 +0200

    -Basic Restful API imprementation: Search Adress get location. -Researching documentation and API requests

---
+++ Create_Minitest/Assets/Carousel/APIController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using UnityEngine;
+using Newtonsoft.Json;
+
+public class APIController : MonoBehaviour
+{
+    private const string API_KEY = "AIzaSyAjQ1IDr8qj9m8hWKO1VRs1hB9FJC1DibM";
+    public string Location = "Paul van Ostaijenlaan 12";
+
+
+    private PlaceSearch GetPlace()
+    {
+        HttpWebRequest request = (HttpWebRequest)WebRequest.Create(
+            string.Format("https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input={0}&fields=place_id,photos,formatted_address,geometry,name&inputtype=textquery&key={1}", Location, API_KEY));
+        HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+        StreamReader reader = new StreamReader(response.GetResponseStream() ?? throw new InvalidOperationException());
+        string jsonResponse = reader.ReadToEnd();
+        var place = JsonConvert.DeserializeObject<PlaceSearch>(jsonResponse);
+        return place;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var loc = GetPlace().Candidates[0].Geometry.Location;
+        print(loc.Lat + ", " + loc.Lng);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Create_Minitest/Assets/Carousel/PlaceSearch.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+[Serializable]
+public class location
+{
+    public double Lat;
+    public double Lng;
+}
+
+[Serializable]
+public class viewport
+{
+    public location Northeast;
+    public location Southwest;
+}
+
+[Serializable]
+    public class geometry
+    {
+        public location Location;
+        public viewport Viewport;
+    }
+[Serializable]
+    public class photos
+    {
+        public int Height;
+        public string Html_attributions;
+        public string Photo_reference;
+        public int Width;
+    }
+
+[Serializable]
+public class candidate
+{
+    public string Formatted_address;
+    public geometry Geometry;
+    public string Name;
+    public string PlaceId;
+    public photos Photos;
+}
+
+[Serializable]
+    public class PlaceSearch
+    {
+        public candidate[] Candidates;
+    }
+
+
